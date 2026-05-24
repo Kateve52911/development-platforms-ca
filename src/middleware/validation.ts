fdbd451710aabe51e1bd18a5b1aca91ac463e553
Kateve52911 <kateve52911@stud.noroff.no>
2026-05-24 17:39:18 +0200

    removed unused code

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,20 +1,5 @@
 import { z } from "zod";
 import { Request, Response, NextFunction } from "express";
-import { loginSchema, registerSchema } from "../schemas/user/user-validation";
-
-/*export const validateRequiredUserData = (
-    req: Request,
-    res: Response,
-    next: NextFunction) => {
-    const result = loginSchema.safeParse(req.body);
-    if (!result.success) {
-        return res.status(400).json({
-            error: "Validation failed",
-            details: result.error.issues.map((issue) => issue.message),
-        });
-    }
-    next();
-}*/
 
 export const validateBody = (schema: z.ZodType) => {
     return (req: Request, res: Response, next: NextFunction) => {
